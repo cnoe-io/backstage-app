b500dbd9039e9b07b0093b5441572788f114f6dc
Manabu Mccloskey <manabu.mccloskey@gmail.com>
2023-06-27 16:49:28 -0700

    update lint rules

--- plugins/argo-workflows/.eslintrc.js
+++ plugins/argo-workflows/.eslintrc.js
@@ -1,1 +1,3 @@
-module.exports = require('@backstage/cli/config/eslint-factory')(__dirname);
+module.exports = require('@backstage/cli/config/eslint-factory')(__dirname, {
+    ignorePatterns: ["src/api/generated/models/**.ts"]
+});
