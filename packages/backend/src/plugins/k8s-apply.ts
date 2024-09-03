9610dd7b150cdae70d45ff1056e020b09f41b680
Mani Kumar Marothu <ama47116@manis-mbp.lan>
2024-09-03 15:05:53 -0700

    Removed cert fields from default config
    Signed-off-by: Mani Kumar Marothu <ama47116@manis-mbp.lan>
    Signed-off-by: Mani Marothu <manikumar.1215@gmail.com>

--- packages/backend/src/plugins/k8s-apply.ts
+++ packages/backend/src/plugins/k8s-apply.ts
@@ -109,7 +109,7 @@ export const createKubernetesApply = (config: Config) => {
                 server: targetCluster.getString('url'),
                 'insecure-skip-tls-verify':
                   !!targetCluster.getOptionalBoolean('skipTLSVerify'),
-              },
+              } satisfies Record<string, any>,
             },
           ],
           users: [
@@ -121,7 +121,7 @@ export const createKubernetesApply = (config: Config) => {
             },
           ],
         };
-        if(!confFile.clusters[0].cluster['insecure-skip-tls-verify']) {
+        if (!confFile.clusters[0].cluster['insecure-skip-tls-verify']) {
           let caDataRaw = targetCluster.getOptionalString('caData');
           if (caDataRaw?.startsWith('-----BEGIN CERTIFICATE-----')) {
             caDataRaw = Buffer.from(
@@ -129,10 +129,13 @@ export const createKubernetesApply = (config: Config) => {
               'utf8',
             ).toString('base64');
           }
-          (confFile.clusters[0].cluster as any)['certificate-authority-data'] =
+          confFile.clusters[0].cluster['certificate-authority-data'] =
             caDataRaw;
-          (confFile.clusters[0].cluster as any)['certificate-authority'] =
-            targetCluster.getOptionalString('caFile');
+          
+          if (! (targetCluster.getOptionalString('caFile').length === 0 || targetCluster.getOptionalString('caFile').length === null)) {
+            confFile.clusters[0].cluster['certificate-authority'] =
+              targetCluster.getOptionalString('caFile');
+          }
         }
         const confString = dumpYaml(confFile);
         const confFilePath = resolveSafeChildPath(ctx.workspacePath, 'config');
